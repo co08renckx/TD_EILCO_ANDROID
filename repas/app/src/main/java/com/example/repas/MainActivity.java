1c4a9b4100b0b4e0fb3b8b435ed4c9fc5289bf58
corentin helinckx <corentin.hel@hotmail.fr>
2021-11-02 15:39:21 +0100

    exo repas fini

--- repas/app/src/main/java/com/example/repas/MainActivity.java
+++ repas/app/src/main/java/com/example/repas/MainActivity.java
@@ -3,25 +3,93 @@ package com.example.repas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText entree;
+    EditText plat;
+
+    boolean fromage;
+    boolean fruit;
+
+    String periodeRepas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //le 1
-        EditText editText = (EditText)  this.findViewById(R.id.entree);
-        editText.setText("New Text");
-        String text = editText.getText().toString();
+        Button valider = (Button) findViewById(R.id.valider);
+        Button annuler = (Button) findViewById(R.id.annuler);
 
-
-        //le 2
-        EditText editText2 = (EditText)  this.findViewById(R.id.plat);
-        editText2.setText("New Text");
-        String text2 = editText2.getText().toString();
     }
+
+    public void validation(View v){
+        //l'entree
+        entree = (EditText) findViewById (R.id.entree);
+        String text_entree = entree.getText().toString();
+
+        //le plat
+        plat = (EditText) findViewById (R.id.plat);
+        String text_plat = plat.getText().toString();
+
+        TextView resumeRepas = (TextView) findViewById(R.id.resumeRepas);
+        resumeRepas.setText("Vous avez mangé "+text_entree+" et "+text_plat);
+
+        //fromage et/ou fruit
+        RadioButton buttonfromage = findViewById(R.id.fromage);
+        fromage = buttonfromage.isChecked();
+
+        RadioButton buttonfruit = findViewById(R.id.fruit);
+        fruit = buttonfruit.isChecked();
+
+        if(fromage){
+            resumeRepas.setText(resumeRepas.getText()+", vous avez pris du fromage");
+        }
+
+        if(fruit){
+            resumeRepas.setText(resumeRepas.getText()+", vous avez pris un fruit");
+        }
+
+        //repas du midi ou du soir ?
+        RadioGroup rg=(RadioGroup)findViewById(R.id.groupButton);
+        periodeRepas = ((RadioButton) findViewById(rg.getCheckedRadioButtonId())).getText().toString();
+        resumeRepas.setText(resumeRepas.getText()+" et vous avez mangé le "+periodeRepas);
+    }
+
+    public void annulation(View v){
+        entree = (EditText) findViewById (R.id.entree);
+        entree.getText().clear();
+
+        //le plat
+        plat = (EditText) findViewById (R.id.plat);
+        plat.getText().clear();
+
+        TextView resumeRepas = (TextView) findViewById(R.id.resumeRepas);
+        resumeRepas.setText("");
+
+        //fromage et/ou fruit
+        RadioButton buttonfromage = findViewById(R.id.fromage);
+        buttonfromage.setChecked(false);
+
+        RadioButton buttonfruit = findViewById(R.id.fruit);
+        buttonfruit.setChecked(false);
+
+        //repas du midi ou du soir ?
+        RadioButton buttonmidi = findViewById(R.id.midi);
+        buttonmidi.setChecked(false);
+
+        RadioButton buttonsoir = findViewById(R.id.soir);
+        buttonsoir.setChecked(false);
+
+    }
+
+
 }
